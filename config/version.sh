bb2f9d56d2e7de02877d17f00b252f854225f6f6
Oscar Mira <valldrac@molly.im>
2026-06-30 13:53:15 +0200

    Hardcode fork version instead of using git tags

--- config/version.sh
+++ config/version.sh
@@ -26,19 +26,21 @@ WEBRTC_VERSION=$(property 'webrtc.version')
 # Given that gclient is run with the "no-history" option, use tags references only.
 WEBRTC_PATCH_REF=$(property 'webrtc.patch.ref' 'extra.properties')
 
+# MOLLY: Resets to 1 every time upstream version changes.
+MOLLY_REVISION="1"
+
 RINGRTC_MAJOR_VERSION=$(property 'ringrtc.version.major')
 RINGRTC_MINOR_VERSION=$(property 'ringrtc.version.minor')
 RINGRTC_REVISION=$(property 'ringrtc.version.revision')
 
 # Specify RingRTC version to publish.
-RINGRTC_VERSION="${RINGRTC_MAJOR_VERSION}.${RINGRTC_MINOR_VERSION}.${RINGRTC_REVISION}"
+RINGRTC_VERSION="${RINGRTC_MAJOR_VERSION}.${RINGRTC_MINOR_VERSION}.${RINGRTC_REVISION}-${MOLLY_REVISION}"
 
 # Release candidate -- for pre-release versions.  Uncomment to use.
 # RC_VERSION="alpha"
 
 # Project version is the combination of the two
-OVERRIDE_VERSION_NO_PREFIX=${OVERRIDE_VERSION#v}
-PROJECT_VERSION="${OVERRIDE_VERSION_NO_PREFIX:-${RINGRTC_VERSION}}${RC_VERSION:+-$RC_VERSION}"
+PROJECT_VERSION="${OVERRIDE_VERSION:-${RINGRTC_VERSION}}${RC_VERSION:+-$RC_VERSION}"
 
 echo "WebRTC : ${WEBRTC_VERSION}"
 echo "RingRTC: ${RINGRTC_VERSION}"
